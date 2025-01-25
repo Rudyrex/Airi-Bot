1517bec54fda751ad528bd18a06e7af82367eca9
DexterZ9 <dexterdp909@gmail.com>
2025-01-25 12:45:26 -0600

    Update exports.js

--- exports.js
+++ exports.js
@@ -3,7 +3,7 @@ export const apis = {
   delirius: 'https://delirius-apiofc.vercel.app/',
   siputzx: 'https://api.siputzx.my.id/api/',
   cloudseek: 'https://cloudseek-api.vercel.app/',
-  dorratz: 'https://api.dorratz.com/',
+  dorratz: 'https://api.dorratz.com/'
 };
 
 const iconUrls = [
