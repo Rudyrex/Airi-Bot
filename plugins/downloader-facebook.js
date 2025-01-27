339e110890eb475a1eb2dec0761f7b8e0369da01
DexterZ9 <dexterdp909@gmail.com>
2025-01-27 13:24:42 -0600

    Update downloader-facebook.js

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -14,8 +14,8 @@ let handler = async (m, { text, conn, args, usedPrefix, command }) => {
         let result1 = await api1.json()
         let downloadUrl1 = result1.urls[0].hd || result1.urls[1].sd;
         
-        await m.reply(downloadUrl1);
-        await conn.sendMessage(m.chat, { video: { url: downloadUrl1 }, fileName: `Facebook.mp4`, mimetype: 'video/mp4', caption: null }, { quoted: m });
+        
+        await await conn.sendMessage(m.chat, { video: { url: downloadUrl1 }, fileName: `Facebook.mp4`, mimetype: 'video/mp4', caption: null }, { quoted: m });
     } catch (e1) {
         try {
             let api2 = await fetch(`${apis.dorratz}fbvideo?url=${fburl}`)    
