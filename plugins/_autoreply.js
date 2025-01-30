8ed01d10cec576fa93a121244147c3b53e3faf90
DexterZ9 <dexterdp909@gmail.com>
2025-01-30 14:58:39 -0600

    Update _autoreply.js

--- plugins/_autoreply.js
+++ plugins/_autoreply.js
@@ -8,13 +8,13 @@ let chat = global.db.data.chats[m.chat]
     
 try {
     if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
-        let gpt = await fetch(`${apis.delirius}ia/gptweb?text=${encodeURIComponent(text)}`)
+        let gpt = await fetch(`${apis.ryzen}api/ai/blackbox?chat=${encodeURIComponent(text)}&options=gpt-4o`)
         let res = await gpt.json()
         await m.reply(res.data)
     } else {
 
         if (!m.fromMe && m.text.match(/airi/gi)) {
-            let gpt = await fetch(`${apis.delirius}ia/gptweb?text=${encodeURIComponent(text)}`)
+            let gpt = await fetch(`${apis.ryzen}api/ai/blackbox?chat=${encodeURIComponent(text)}&options=gpt-4o`)
             let res = await gpt.json()
             await m.reply(res.data)
         }
