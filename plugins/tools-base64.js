2bb5ac7b1a5d4871f2de1ecd49ba333108ada38b
DexterZ9 <dexterdp909@gmail.com>
2025-02-01 11:41:22 -0600

    Update tools-base64.js

--- plugins/tools-base64.js
+++ plugins/tools-base64.js
@@ -1,13 +1,16 @@
 let handler = async (m, { args }) => {
-  if (m.quoted && m.quoted.text) {
-    if (/^enc(rypt)?$/i.test(args[0])) m.reply(Buffer.from(m.quoted.text, 'utf-8').toString('base64'))
-    else if (/^dec(rypt)?$/i.test(args[0])) m.reply(Buffer.from(m.quoted.text, 'base64').toString('utf-8'))
-  } else if (args.length >= 2) {
-    if (/^enc(rypt)?$/i.test(args[0])) m.reply(Buffer.from(args.slice(1).join(' '), 'utf-8').toString('base64'))
-    else if (/^dec(rypt)?$/i.test(args[0])) m.reply(Buffer.from(args.slice(1).join(' '), 'base64').toString('utf-8'))
-  } else throw false
+    
+    if (m.quoted && m.quoted.text) {
+        if (/^enc(rypt)?$/i.test(args[0])) m.reply(Buffer.from(m.quoted.text, 'utf-8').toString('base64'))
+        else if (/^dec(rypt)?$/i.test(args[0])) m.reply(Buffer.from(m.quoted.text, 'base64').toString('utf-8'))
+    } else if (args.length >= 2) {
+        if (/^enc(rypt)?$/i.test(args[0])) m.reply(Buffer.from(args.slice(1).join(' '), 'utf-8').toString('base64'))
+        else if (/^dec(rypt)?$/i.test(args[0])) m.reply(Buffer.from(args.slice(1).join(' '), 'base64').toString('utf-8'))
+    } else {
+        m.reply(`${em} *Agrega o responde a un texto para encriptar o desencriptar*\n\n*Uso:*\n_.base64 <enc/dec> <texto>_\n\n*Ejemplo:*\n_.base64 enc hola_`);
+    }
+  
 }
-handler.tags = ['tools']
-handler.help = ['base64 <enc/dec>']
+
 handler.command = /^base64$/i
 export default handler
