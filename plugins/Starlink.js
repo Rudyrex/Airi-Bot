5de9f4410b506bb3643123ce316ce89374dcce0e
DexterZ9 <dexterdp909@gmail.com>
2025-02-05 09:48:42 -0600

    Update Starlink.js

--- plugins/Starlink.js
+++ plugins/Starlink.js
@@ -5,7 +5,7 @@ import fetch from 'node-fetch';
 let handler = async (m, { conn }) => {
     let args = m.text.split(' ').slice(1);
     try {
-        let { title, size, quality, thumbnail, dl_url } = await Starlights.ytmp4(args[0]);
+        let { dl_url } = await Starlights.ytmp4(args[0]);
         await m.reply(dl_url);
     } catch (error) {
         await m.reply(`¡Uy! Parece que hubo un error: ${error}`);
