96127fe2e766f1d1bc870b4b2bb8aa55a9546d3c
DexterZ9 <dexterdp909@gmail.com>
2025-02-19 12:43:30 -0600

    Update and rename downloader-ytmp4doc.js to download-ytmp4.js

---
+++ plugins/download-ytmp4.js
@@ -0,0 +1,23 @@
+import { apis } from '../exports.js';
+import fetch from 'node-fetch';
+
+
+let handler = async (m, { text, conn, args, usedPrefix, command }) => {
+    
+    if (!args[0]) return await conn.reply(m.chat, `${em} Agrega un enlace de Youtube`, m);
+    let yturl = args[0]
+    m.react('⏳')
+    
+    try {
+        let api1 = await fetch(`${apis.random1}youtube-video?url=${yturl}`)    
+        let result1 = await api1.json()
+        let downloadUrl1 = result1.result.downloadUrl;
+        
+        await conn.sendMessage(m.chat, {document: {url: downloadUrl1}, caption: null, mimetype: 'video/mp4', fileName: `video.mp4`}, {quoted: m});
+    } catch (e1) {
+        m.react('❌')
+    }
+};
+
+handler.command = ['ytv', 'ytmp4', 'ytvdoc'];
+export default handler;
