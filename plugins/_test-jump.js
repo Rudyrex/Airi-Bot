54306d91d0b81e2b204df99e92e26d6d81691015
DexterZ9 <dexterdp909@gmail.com>
2025-03-04 12:56:43 -0600

    Update _test-jump.js

--- plugins/_test-jump.js
+++ plugins/_test-jump.js
@@ -1,5 +1,10 @@
 let handler = async (m, { conn }) => {
-    m.react('🐟');
+    m.react('🐟'); // Reacciona con el emoji de pez
+
+    let user = m.sender; // Obtiene el ID del usuario que envió el mensaje
+    let tag = `@${user.replace(/@.+/, '')}`; // Formatea el ID para etiquetar en el mensaje
+
+    conn.reply(m.chat, `${tag} te desafía a un duelo de Magikarp Jump!`, m, { mentions: [user] });
 }
 
 // Detecta varias expresiones sin prefijo
