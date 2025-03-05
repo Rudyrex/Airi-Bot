510639f0b1540e3619c05f8993c225c0d108fa33
DexterZ9 <dexterdp909@gmail.com>
2025-03-05 13:01:51 -0600

    Update _testjump.js

--- plugins/_testjump.js
+++ plugins/_testjump.js
@@ -1,7 +1,12 @@
 let handler = async (m, { conn }) => {
     if (m.fromMe) return; // Evita que el bot se responda a sí mismo
     
+    let user = global.db.data.users[m.sender];
 
+    if (!user.peces || user.peces.length === 0) {
+        return m.reply("No tienes Magikarps para iniciar un duelo.");
+    }
+    
     let user = m.sender; // Obtiene el ID del usuario que envió el mensaje
     let tag = `@${user.replace(/@.+/, '')}`; // Formatea el ID para etiquetar en el mensaje
 
