325ac6da8d6f94e56a79a659485249299f24e9bf
DexterZ9 <dexterdp909@gmail.com>
2025-03-05 14:15:22 -0600

    Update _testjump.js

--- plugins/_testjump.js
+++ plugins/_testjump.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn }) => {
     let user = m.sender; // Obtiene el ID del usuario que envió el mensaje
     let tag = `@${user.replace(/@.+/, '')}`; // Formatea el ID para etiquetar en el mensaje
 
-    conn.reply(m.chat, `${tag} te desafía a un duelo de Magikarp Jump!`, m, { mentions: [user] });
+    conn.reply(m.chat, `${tag} te desafía a un duelo de Magikarp Jump!`, m);
 }
 
 // Detecta varias expresiones sin prefijo
