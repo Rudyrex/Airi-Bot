ad445269d6716e84c3d15c2b9befee453d9265d4
DexterZ9 <dexterdp909@gmail.com>
2025-03-08 16:26:54 -0600

    Update _testlick.js

--- plugins/_testlick.js
+++ plugins/_testlick.js
@@ -1,54 +1,38 @@
+import { getUserName } from '../exports.js'
 
 let handler = async (m, { conn }) => {
     let chat = global.db.data.chats[m.chat]
     
     if (chat.nsfw) {
+        const mentionedJid = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted?.sender;
     
-    const mentionedJid = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted?.sender;
+        if (!mentionedJid) return m.reply(`${em} *Debes mencionar o etiquetar a alguien para follar*`);
+        
+        const senderJid = m.sender;
+        
+        const senderName = await getUserName(conn, senderJid);
+        const mentionedName = await getUserName(conn, mentionedJid);
+
+        // Arreglo con URLs de videos
+        const videos = [
+            'https://files.catbox.moe/8wgme9.mp4',
+            'https://files.catbox.moe/akh0e1.mp4',
+            'https://files.catbox.moe/2sletl.mp4'
+        ];
     
-    if (!mentionedJid) return m.reply(`${em} *Debes mencionar o etiquetar a alguien para follar*`);
-
-    const senderJid = m.sender;
-
-    // Función para obtener el nombre del usuario
-    async function getUserName(conn, jid) {
-        // Intenta obtener el nombre del objeto global
-        let name = global.db.data.users[jid]?.name;
-        // Si no se encuentra, intenta usar la API de conexión
-        if (!name) {
-            name = await conn.getName(jid);
-            // Si aún no se encuentra, intenta obtener desde el contacto
-            if (!name) {
-                const contact = await conn.fetchContact(jid);
-                name = contact?.notify || contact?.name || jid.split('@')[0];
-            }
+        const randomVideo = videos[Math.floor(Math.random() * videos.length)];
+        const text 
+        // Enviar el mensaje con el video
+        if ( mentionedJid == conn.user.jid ) {
+            await conn.sendMessage(m.chat, { video: { url: randomVideo }, caption: `🥵 *${senderName}* se folló a... omitiremos eso`, gifPlayback: true, mentions: [senderJid] }, { quoted: m });    
+        } else {
+            await conn.sendMessage(m.chat, { video: { url: randomVideo }, caption: `🥵 *${senderName}* se folló a *${mentionedName}*`, gifPlayback: true, mentions: [senderJid, mentionedJid] }, { quoted: m });    
         }
-        return name;
-    }
-
-    const senderName = await getUserName(conn, senderJid);
-    const mentionedName = await getUserName(conn, mentionedJid);
-
-    // Arreglo con URLs de videos
-    const videos = [
-        'https://files.catbox.moe/8wgme9.mp4',
-        'https://files.catbox.moe/akh0e1.mp4',
-        'https://files.catbox.moe/2sletl.mp4'
-    ];
-    const randomVideo = videos[Math.floor(Math.random() * videos.length)];
-
-    // Enviar el mensaje con el video
-    await conn.sendMessage(m.chat, {
-        video: { url: randomVideo },
-        caption: `🥵 *${senderName}* se folló a *${mentionedName}* 💦`,
-        gifPlayback: true, // Reproducción automática si es un gif animado
-        mentions: [senderJid, mentionedJid]
-    }, { quoted: m });
     } else {
         m.reply(`${em} *Los comandos +18 estan desactivados*`);
     }
 };
 
 handler.command = ['fuck', 'follar'];
+handler.group = true;
 export default handler;
-    
