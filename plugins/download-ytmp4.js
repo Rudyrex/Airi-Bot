42893e47d71e851ac85f10f2160c77a45785a103
DexterZ9 <dexterdp909@gmail.com>
2025-03-11 13:39:38 -0600

    Update download-ytmp4.js

--- plugins/download-ytmp4.js
+++ plugins/download-ytmp4.js
@@ -1,24 +1,44 @@
-import { apis } from '../exports.js';
+import { ytdl } from 'savetubedl';
 import fetch from 'node-fetch';
 
+const getYoutubeId = (url) => {
+    const regex = /(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?/\s]{11})/;
+    const matches = url.match(regex);
+    return matches ? matches[1] : null;
+};
 
 let handler = async (m, { text, conn, args, usedPrefix, command }) => {
+    if (!args[0]) return await conn.reply(m.chat, `${em} *Agrega un enlace de YouTube*_\n\n_Ejemplo:_\n.${command} https://www.youtube.com`, m);
     
-    if (!args[0]) return await conn.reply(m.chat, `${em} Agrega un enlace de Youtube`, m);
-    let yturl = args[0]
-    m.react('⏳')
+    let youtubeLink = '';
+    
+    if (args[0].includes('you')) {
+        youtubeLink = args[0];
+    } else {
+        return await conn.reply(m.chat, `${em} *El enlace no es de YouTube*`, m);
+    }
+    
+    
+    const isShort = youtubeLink.includes('youtube.com/shorts/');
+    const videoId = getYoutubeId(youtubeLink);
+    
+    
+    const shortYoutubeUrl = isShort ? youtubeLink : `https://youtu.be/${videoId}`;
+    
+    m.react('⏳');
     
     try {
-        let api1 = await fetch(`${apis.random1}youtube-video?url=${yturl}`)    
-        let result1 = await api1.json()
-        let downloadUrl1 = result1.result.downloadUrl;
-        let title1 = result1.result.title
-        
-        await conn.sendMessage(m.chat, {document: {url: downloadUrl1}, caption: null, mimetype: 'video/mp4', fileName: `${title1}.mp4`}, {quoted: m});
+    //╔────── ¤ ◎ savetubedl ◎ ¤ ──────╗
+        let result = await ytdl(shortYoutubeUrl, '360');
+        let title = result.response.titulo;
+        let downloadUrl = result.response.descarga;
+        await conn.sendMessage(m.chat, {document: {url: downloadUrl}, caption: null, mimetype: 'video/mp4', fileName: `${title}.mp4`}, {quoted: m});
+    //╚────── ¤ ◎ node-yt-dl ◎ ¤ ──────╝
     } catch (e1) {
-        m.react('❌')
+        m.react(❌);
     }
 };
 
-handler.command = ['ytv', 'ytmp4', 'ytvdoc'];
+handler.command = ['ytmp4', 'ytv'];
 export default handler;
+                                                                                                                                        
