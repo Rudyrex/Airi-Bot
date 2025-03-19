e8db26989e384c1458b4f4d964f09516d365720c
DexterZ9 <dexterdp909@gmail.com>
2025-03-19 07:08:54 -0600

    Update download-ytmp3.js

--- plugins/download-ytmp3.js
+++ plugins/download-ytmp3.js
@@ -1,5 +1,5 @@
-import { ytdlaud } from 'savetubedl';
 import fetch from 'node-fetch';
+
 
 const getYoutubeId = (url) => {
     const regex = /(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?/\s]{11})/;
@@ -8,37 +8,54 @@ const getYoutubeId = (url) => {
 };
 
 let handler = async (m, { text, conn, args, usedPrefix, command }) => {
-    if (!args[0]) return await conn.reply(m.chat, `${em} *Agrega un enlace de YouTube*_\n\n_Ejemplo:_\n.${command} https://www.youtube.com`, m);
-    
-    let youtubeLink = '';
-    
-    if (args[0].includes('you')) {
-        youtubeLink = args[0];
-    } else {
-        return await conn.reply(m.chat, `${em} *El enlace no es de YouTube*`, m);
+    if (!args[0]) {
+        return await conn.reply(m.chat, '*Agrega un enlace de YouTube*.\n\n_Ejemplo:_\n' + `.${command} https://www.youtube.com/watch?v=abcd1234`, m);
     }
     
+    let youtubeLink = args[0];
     
+    if (!youtubeLink.includes('youtube.com') && !youtubeLink.includes('youtu.be')) {
+        return await conn.reply(m.chat, `${em} *El enlace no es de YouTube.*`, m);
+    }
+
     const isShort = youtubeLink.includes('youtube.com/shorts/');
     const videoId = getYoutubeId(youtubeLink);
-    
-    
     const shortYoutubeUrl = isShort ? youtubeLink : `https://youtu.be/${videoId}`;
-    
+
     m.react('⏳');
-    
+
     try {
-    //╔────── ¤ ◎ savetubedl ◎ ¤ ──────╗
-        let result = await ytdlaud(shortYoutubeUrl);
-        let title = result.response.titulo;
-        let downloadUrl = result.response.descarga;
-        await conn.sendMessage(m.chat, {document: {url: downloadUrl}, caption: null, mimetype: 'audio/mpeg', fileName: `${title}.mp3`}, {quoted: m});
-    //╚────── ¤ ◎ node-yt-dl ◎ ¤ ──────╝
-    } catch (e1) {
+        
+        const apiUrl = `https://cloudseek-api.vercel.app/y2loader?url=${encodeURIComponent(shortYoutubeUrl)}&format=mp3`;
+        const response = await fetch(apiUrl);
+        const result = await response.json();
+
+        if (!result.status) {
+            m.react('❌');
+            return await conn.reply(m.chat, '❌ No se pudo obtener el enlace de descarga.', m);
+        }
+
+        let title = result.title || 'video';
+        let downloadUrl = result.downloadUrl;
+
+        
+        await conn.sendMessage(
+            m.chat, 
+            { 
+                document: { url: downloadUrl },
+                mimetype: 'audio/mpeg', 
+                fileName: `${title}.mp3`
+            }, 
+            { quoted: m }
+        );
+
+        m.react('✅');
+    } catch (e) {
+        console.error(e);
         m.react('❌');
     }
 };
 
 handler.command = ['ytmp3', 'yta'];
 export default handler;
-    
+        
